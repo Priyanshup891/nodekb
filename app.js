8e57402327b35a07831a49db199d12d5f4b904cd
Priyanshup891 <priyanshup891@gmail.com>
2022-04-30 15:01:48 +0530

    layout

--- app.js
+++ app.js
@@ -6,8 +6,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+  let articles = [
+    {
+      id:1,
+      title:"Article One",
+      author:"Priyanshu Patil",
+      body:"This is a article one"
+    },
+    {
+      id:2,
+      title:"Article Two",
+      author:"Priyanshu Patil",
+      body:"This is a article two"
+    },
+    {
+      id:3,
+      title:"Article Three",
+      author:"Priyanshu Patil",
+      body:"This is a article three"
+    }
+  ];
   res.render('index', {
-    title:"index"
+    articles: articles
+  })
+})
+
+app.get('/articles/add', (req, res) => {
+  res.render('add', {
+    title:"Add Articles"
   })
 })
 
